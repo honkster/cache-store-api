6dc2234e5e45ed95d4e00dd083c68f0041e2ce88
Scott Gonyea <sgonyea@truecar.com>
2011-09-02 16:24:16 -0700

    Moved the before/stubbing to the csa_spec.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,28 +1,10 @@
 require "rubygems"
+require "bundler/setup"
 
-require "rspec"
+Bundler.require :default, :development
 
-$LOAD_PATH.unshift(File.expand_path("#{File.dirname(__FILE__)}/../lib"))
 require "cache-store-api"
-require "cache-store-api/test_cache"
 
 RSpec.configure do |configuration|
   configuration.mock_with :rr
-
-  configuration.before do
-    @cache = Object.new
-
-    stub(cache).exist? {raise "You should mock me."}
-    stub(cache).read {raise "You should mock me."}
-    stub(cache).write {raise "You should mock me."}
-    stub(cache).delete {raise "You should mock me."}
-
-    CacheStoreApi.set_cache do
-      @cache
-    end
-  end
-end
-
-RSpec::Core::ExampleGroup.class_eval do
-  attr_reader :cache
 end
