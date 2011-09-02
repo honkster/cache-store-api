dbd0c5d05f67c0e2ed11ca409900c95719470482
Scott Gonyea <sgonyea@truecar.com>
2011-09-02 16:34:28 -0700

    Added tests for the disabling of caching.  Memo-ized "CacheStoreApi" to "cache_store_api" which is re-initialized fore every block.  Just to make sure tests do not pollute each other.

--- spec/cache-store-api_spec.rb
+++ spec/cache-store-api_spec.rb
@@ -1,45 +1,111 @@
 require "#{File.dirname(__FILE__)}/spec_helper"
 
 describe CacheStoreApi do
+  let(:cache) do
+    Object.new.tap do |cache|
+      stub(cache).exist?  { raise "You should mock me." }
+      stub(cache).read    { raise "You should mock me." }
+      stub(cache).write   { raise "You should mock me." }
+      stub(cache).delete  { raise "You should mock me." }
+    end
+  end
+
+  let(:cache_store_api) do
+    Module.new do
+      include CacheStoreApi
+      extend  CacheStoreApi::ClassMethods
+    end
+  end
+
+  before(:each) do
+    cache_store_api.set_cache { cache }
+  end
+
+  describe ".disable_cache!" do
+    it "should set the @caching_status instance variable to :disabled" do
+      cache_store_api.disable_cache!
+      cache_store_api.instance_variable_get(:@caching_status).should be(:disabled)
+    end
+  end
+
+  describe ".enable_cache!" do
+    it "should set the @caching_status instance variable to :enabled" do
+      cache_store_api.enable_cache!
+      cache_store_api.instance_variable_get(:@caching_status).should be(:enabled)
+    end
+  end
+
+  describe ".caching?" do
+    it "should set the @caching_status instance variable to :enabled, if not already set" do
+      cache_store_api.instance_variable_get(:@caching_status).should be_nil
+      cache_store_api.caching?
+      cache_store_api.instance_variable_get(:@caching_status).should be(:enabled)
+    end
+
+    it "should reflect the value of @caching_status" do
+      cache_store_api.caching?.should be_true
+
+      cache_store_api.disable_cache!
+
+      cache_store_api.caching?.should be_false
+    end
+  end
+
   describe ".lazy_cache" do
-    context "when a block is given" do
-      context "when the key exists in the cache" do
-        it "returns the value of cache.read" do
-          mock(cache).exist?("the-key") {true}
-          mock(cache).read("the-key") {"the value"}
+    context "when caching is disabled" do
+      context "when a block is given" do
+        before(:each) do
+          cache_store_api.disable_cache!
 
-          CacheStoreApi.lazy_cache("the-key") {"new value"}.should == "the value"
+          stub(cache).exist?("cached-key") { fail "Cache should not be read from, when disabled!" }
         end
 
-      end
-
-      context "when the key does not exist in the cache" do
-        context "when an expiration is not given" do
-          it "returns the return value of the block and writes that value (expiring in one hour) into the cache" do
-            mock(cache).exist?("the-key") {false}
-            mock(cache).write("the-key", "new value", :expires_in => 3600)
-
-            CacheStoreApi.lazy_cache("the-key") {"new value"}.should == "new value"
-          end
+        it "always yields to the given block" do
+          cache_store_api.lazy_cache("cached-key") { "some value" }.should == "some value"
         end
-
-        context "when an expiration is given" do
-          it "returns the return value of the block and writes that value (expiring in the given time) into the cache" do
-            mock(cache).exist?("the-key") {false}
-            mock(cache).write("the-key", "new value", :expires_in => 7200)
-
-            CacheStoreApi.lazy_cache("the-key", 7200) {"new value"}.should == "new value"
-          end
-        end
-
       end
     end
 
-    context "when a block is not given" do
-      it "returns the value of cache.read" do
-        mock(cache).read("the-key") {"the value"}
+    context "when caching is enabled" do
+      context "when a block is given" do
+        context "when the key exists in the cache" do
+          it "returns the value of cache.read" do
+            mock(cache).exist?("the-key") {true}
+            mock(cache).read("the-key") {"the value"}
 
-        CacheStoreApi.lazy_cache("the-key").should == "the value"
+            cache_store_api.lazy_cache("the-key") {"new value"}.should == "the value"
+          end
+
+        end
+
+        context "when the key does not exist in the cache" do
+          context "when an expiration is not given" do
+            it "returns the return value of the block and writes that value (expiring in one hour) into the cache" do
+              mock(cache).exist?("the-key") {false}
+              mock(cache).write("the-key", "new value", :expires_in => 3600)
+
+              cache_store_api.lazy_cache("the-key") {"new value"}.should == "new value"
+            end
+          end
+
+          context "when an expiration is given" do
+            it "returns the return value of the block and writes that value (expiring in the given time) into the cache" do
+              mock(cache).exist?("the-key") {false}
+              mock(cache).write("the-key", "new value", :expires_in => 7200)
+
+              cache_store_api.lazy_cache("the-key", 7200) {"new value"}.should == "new value"
+            end
+          end
+
+        end
+      end
+
+      context "when a block is not given" do
+        it "returns the value of cache.read" do
+          mock(cache).read("the-key") {"the value"}
+
+          cache_store_api.lazy_cache("the-key").should == "the value"
+        end
       end
     end
   end
@@ -47,7 +113,7 @@ describe CacheStoreApi do
   describe ".expire" do
     it "deletes the key in the cache" do
       mock(cache).delete("the-key")
-      CacheStoreApi.expire("the-key")
+      cache_store_api.expire("the-key")
     end
   end
 
@@ -55,17 +121,12 @@ describe CacheStoreApi do
     it "returns the @data variable of the cache" do
       data = {}
       cache.instance_variable_set(:@data, data)
-      CacheStoreApi.data.should == data
+      cache_store_api.data.should == data
     end
   end
 
   describe CacheStoreApi::TestCache do
-    before do
-      @cache = CacheStoreApi::TestCache.new
-      CacheStoreApi.set_cache do
-        cache
-      end
-    end
+    let(:cache) { CacheStoreApi::TestCache.new }
 
     describe ".lazy_cache" do
       context "when a block is given" do
@@ -74,7 +135,7 @@ describe CacheStoreApi do
             cache.write("the-key", "the value")
             cache.exist?("the-key").should be_true
 
-            CacheStoreApi.lazy_cache("the-key") {"new value"}.should == "the value"
+            cache_store_api.lazy_cache("the-key") {"new value"}.should == "the value"
           end
 
         end
@@ -84,7 +145,7 @@ describe CacheStoreApi do
             it "returns the return value of the block and writes that value (expiring in one hour) into the cache" do
               cache.exist?("the-key").should be_false
 
-              CacheStoreApi.lazy_cache("the-key") {"new value"}.should == "new value"
+              cache_store_api.lazy_cache("the-key") {"new value"}.should == "new value"
             end
           end
 
@@ -92,7 +153,7 @@ describe CacheStoreApi do
             it "returns the return value of the block and writes that value (expiring in the given time) into the cache" do
               cache.exist?("the-key").should be_false
 
-              CacheStoreApi.lazy_cache("the-key", 7200) {"new value"}.should == "new value"
+              cache_store_api.lazy_cache("the-key", 7200) {"new value"}.should == "new value"
             end
           end
 
@@ -103,7 +164,7 @@ describe CacheStoreApi do
         it "returns the value of cache.read" do
           cache.write("the-key", "the value")
 
-          CacheStoreApi.lazy_cache("the-key").should == "the value"
+          cache_store_api.lazy_cache("the-key").should == "the value"
         end
       end
     end
@@ -112,14 +173,14 @@ describe CacheStoreApi do
       it "deletes the key in the cache" do
         cache.write("the-key", "the value")
         cache.exist?("the-key").should be_true
-        CacheStoreApi.expire("the-key")
+        cache_store_api.expire("the-key")
         cache.exist?("the-key").should be_false
       end
     end
 
     describe ".data" do
       it "returns the @data variable of the cache" do
-        CacheStoreApi.data.should == cache.data
+        cache_store_api.data.should == cache.data
       end
     end
   end
